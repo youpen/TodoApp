574e812563dad9909986198965e54034c0cc51e9
youpen <wu.yupeng@foxmail.com>
2019-08-04 02:56:13 +0800

    add logo

--- app/src/main/java/com/example/yupeng/todoapp/TaskListFragment.java
+++ app/src/main/java/com/example/yupeng/todoapp/TaskListFragment.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -35,20 +37,43 @@ public class TaskListFragment extends Fragment {
 
     }
 
+    // 这个方法是创建adapter， 即在recyclerView中插入数据
     private void updateUI() {
-        TaskLab taskLab = TaskLab.get(getActivity());
+        TaskLab taskLab = TaskLab.get(getActivity()); // 单例
         List<Task> tasks = taskLab.getTasks();
 
         mTaskAdapter = new TaskAdapter(tasks);
         mRecyclerView.setAdapter(mTaskAdapter);
     }
 
-    private class TaskHolder extends RecyclerView.ViewHolder {
+    private class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+//        implements View.OnClickListener {}
+
+        private TextView mTaskTitleTextView;
+        private TextView mTaskDateTextView;
+
+        private Task mTask;
+
         public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.task_list_item, parent, false)); // 这里为什么要加false
+            mTaskTitleTextView = itemView.findViewById(R.id.task_title_id);
+            mTaskDateTextView = itemView.findViewById(R.id.task_date_id);
+            itemView.setOnClickListener(this);
+        }
+        public void bind(Task task) {
+            mTask = task;
+            mTaskDateTextView.setText(mTask.getDate().toString());
+            mTaskTitleTextView.setText(mTask.getTitle());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getContext(), "111", Toast.LENGTH_SHORT).show();
         }
     }
 
+    // adapter负责创建Item，绑定item
     private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
 
         private List<Task> mTasks;
@@ -56,7 +81,14 @@ public class TaskListFragment extends Fragment {
             mTasks = tasks;
         }
 
+//        @Override
+//        // TODO
+//        public int getItemViewType(int position) {
+//            return super.getItemViewType(position);
+//        }
+
         @Override
+        // 这个方法创建到一定数量item后就不调用了，改用pool内的item复用来绑定
         public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             return new TaskHolder(layoutInflater, parent);
@@ -65,6 +97,8 @@ public class TaskListFragment extends Fragment {
         @Override
 //        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) { // TODO　？？？
         public void onBindViewHolder(TaskHolder holder, int position) {
+            Task task = mTasks.get(position);
+            holder.bind(task);
         }
 
         @Override
